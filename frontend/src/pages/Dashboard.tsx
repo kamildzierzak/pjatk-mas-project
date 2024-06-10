f43730aaf5c72e1d6c8ba1a69dc5d64cfd818d62
Kamil Dzierzak <kamdzierzak@gmail.com>
2024-06-10 23:42:37 +0200

    feat(frontend): add tailwindcss, daisyui and simple navigation component chore(frontend): clean up unused files

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className="p-10">
+        <div className="w-full flex flex-col justify-center items-center gap-5">
+          <h1 className="text-5xl mt-10">Welcome to WarManSystem!</h1>
+          <h2 className="text-xl">
+            Best warehouse management system for plants!
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
